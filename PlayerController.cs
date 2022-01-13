6343296a779407a6b585352bdb4270abdc4c3a68
unnoteduser <blrpotato@gmail.com>
2022-01-13 16:50:39 +0200

    first commit

---
+++ PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRb; 
+    private GameObject focalPoint;
+    public GameObject powerupIndicator;
+    public float speed = 5.0f;
+    float powerupStrenght = 15.0f;
+    public bool hasPowerup = false;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal point");
+    }
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other) // use OnTrigger when we want to know that sth is collide with each other
+    {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            powerupIndicator.gameObject.SetActive(true);
+            StartCoroutine(PowerupCountdownRoutin());// start PowerupCountdownRoutin thread below
+        }
+    }
+    IEnumerator PowerupCountdownRoutin() //enable countdown timer outside our update loop
+    {
+         yield return new WaitForSeconds(7);// use keyword yield and WaitForSec method to wait before we do smt
+         hasPowerup = false; // this happen when countdown over
+         powerupIndicator.gameObject.SetActive(false);
+    }
+    private void OnCollisionEnter(Collision collision) // use OnCollision when we work with physics
+    {   
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);// direction defines through subtraction enemy position from player position
+            
+            enemyRb.AddForce(awayFromPlayer * powerupStrenght, ForceMode.Impulse);
+            Debug.Log("Collided with" + collision.gameObject.name + "with powerup set to" + hasPowerup); //concatenation
+        }
+    }
+}
---
+++ SpawnManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+    private float spawnRange = 10.0f;
+    public int enemyCount;
+    public int waveNumber = 1; 
+    void Start()
+    {
+        SpawnEnemyWaves(waveNumber);
+    }
+
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber ++;
+            SpawnEnemyWaves(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
+    void SpawnEnemyWaves(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+    private Vector3 GenerateSpawnPosition()
+    {
+         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+         float spawnPosX = Random.Range(-spawnRange, spawnRange);
+         Vector3 randomPos = new Vector3 (spawnPosX, 0, spawnPosZ);
+
+         return randomPos;
+    }
+}
